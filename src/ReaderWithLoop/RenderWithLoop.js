8120bc790bc5507464a1d408072557b0063d0630
thanhlong2023 <caolong770709@gmail.com>
2025-02-22 16:29:36 +0700

    16  render with map

--- src/ReaderWithLoop/RenderWithLoop.js
+++ src/ReaderWithLoop/RenderWithLoop.js
@@ -7,7 +7,7 @@ export default class RenderWithLoop extends Component {
         { id: 3, name: 'silver car', price: 3000, img: './carbasic/products/silver-car.jpg' },
         { id: 4, name: 'steel car', price: 4000, img: './carbasic/products/steel-car.jpg' }
     ]
-
+    // Cách 1: Render array object sử dụng for
     renderTable = () => {
         let mangTrCpmponet = [];
         for (let index = 0; index < this.productList.length; index++) {
@@ -25,6 +25,34 @@ export default class RenderWithLoop extends Component {
         }
         return mangTrCpmponet;
     }
+    // Cách 2: Render array object sử dụng map
+    renderTable2 = () => {
+        // let mangTrComponent = this.productList.map((product, index) => {
+        //     return (
+        //         <tr key={index}>
+        //             <td>{product.id}</td>
+        //             <td>{product.name}</td>
+        //             <td>{product.price}</td>
+        //             <td><img src={product.img} alt={product.name} style={{ width: '100px' }} /></td>
+        //             <td><button className='btn btn-success'>Detail</button></td>
+        //         </tr>
+        //     )
+        // })
+        // return mangTrComponent;
+
+        return this.productList.map((product, index) => {
+            return (
+                <tr key={index}>
+                    <td>{product.id}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td><img src={product.img} alt={product.name} style={{ width: '100px' }} /></td>
+                    <td><button className='btn btn-success'>Detail</button></td>
+                </tr>
+            )
+        })
+    }
+
     render() {
         return (
             <div className='container-fluid'>
@@ -39,7 +67,8 @@ export default class RenderWithLoop extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.renderTable()}
+                        {/* {this.renderTable()} */}
+                        {this.renderTable2()}
                     </tbody>
                 </table>
             </div>
